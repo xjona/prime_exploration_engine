086e15e54ad71ae20faeaf05a271bfa19fb76708
jona <67206291+xjona@users.noreply.github.com>
2020-07-08 21:16:14 +0200

    Add files via upload

---
+++ Prime_Numbers.py
@@ -0,0 +1,20 @@
+textdoc = open("My_favourite_Primes.txt","w+")
+
+def PrimeGenerator(many):
+    textdoc.write("Primenumbers under " + str(many) + "\n")
+    prim =[2]
+    for i in range(2,many-2):
+        temp = True
+        for j in prim:
+            if i%j == 0:
+                temp = False
+        if temp == True:
+            prim.append(i)
+    print(prim)
+
+    for p in prim:
+        textdoc.write("No." + str(prim.index(p)+1) + "   " + str(p) + "\n")
+
+PrimeGenerator(10000)
+
+textdoc.close()
